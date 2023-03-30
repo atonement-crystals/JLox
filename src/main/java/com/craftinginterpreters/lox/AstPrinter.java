5837f39822d6b8ccb6a14082bc98958cb2ae47ee
Aaron Carson <aaroncarsonart@gmail.com>
2023-03-30 12:40:19 -0500

    https://craftinginterpreters.com/functions.html

--- src/main/java/com/craftinginterpreters/lox/AstPrinter.java
+++ src/main/java/com/craftinginterpreters/lox/AstPrinter.java
@@ -18,6 +18,12 @@ public class AstPrinter implements Expr.Visitor<String> {
     }
 
     @Override
+    public String visitCallExpr(Expr.Call expr) {
+        // TODO: Unimplemented.
+        return null;
+    }
+
+    @Override
     public String visitGroupingExpr(Expr.Grouping expr) {
         return parenthesize("group", expr.expression);
     }
